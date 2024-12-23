ffae6524fc8ff2e148ad33937c109ba5e70412e2
mustafa <mustafaqasimi8656@gmail.com>
2024-12-23 15:16:01 +0330

    feat the input function

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculator {
+    public void takingInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+        System.out.print("Choose an operator:(+,-,*,/) ");
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
