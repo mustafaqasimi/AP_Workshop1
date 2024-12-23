36a3cf300d6d56732b5dcb092de3dfb31d820efc
mustafa <mustafaqasimi8656@gmail.com>
2024-12-23 15:40:05 +0330

    the caculation methode added

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,10 +10,31 @@ public class Calculator {
         System.out.print("Enter the second number: ");
         int secondNumber = scanner.nextInt();
         System.out.print("Choose an operator:(+,-,*,/) ");
+        char operator = scanner.next().charAt(0);
+        calculation(firstNumber,operator,secondNumber);
+
+
+    }
+    public void calculation(int firstNumber, char operator ,int secondNumber){
+        switch(operator){
+            case '+' ->System.out.print("the sum of "+firstNumber+" and "+secondNumber+" is:" + (firstNumber+secondNumber));
+            case '-'->System.out.println("the minus of "+firstNumber+" and "+secondNumber +" is:" + (firstNumber - secondNumber));
+            case '*'->System.out.println("the multiplication of "+firstNumber+" and "+secondNumber+" is:" + (firstNumber * secondNumber));
+            case '/'->
+                System.out.println("the division of "+firstNumber+" and "+secondNumber+" is:" + (firstNumber / secondNumber));
+
+
+            default -> System.out.println("Invalid operator");
+
+
+        }
+
     }
 
 
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.takingInput();
 
     }
 }
